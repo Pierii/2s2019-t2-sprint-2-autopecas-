3332517bd4f248a56b68ac3a7a418da7d40ac1e5
Rafael Pieri Almeida <46181942866@info.local>
2019-09-04 11:21:51 -0300

    04.09.2019T11:21

--- Senai.AutoPecas.WebApi/Senai.AutoPecas.WebApi/Startup.cs
+++ Senai.AutoPecas.WebApi/Senai.AutoPecas.WebApi/Startup.cs
@@ -6,15 +6,63 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Senai.AutoPecas.WebApi
 {
     public class Startup
+{
+    // This method gets called by the runtime. Use this method to add services to the container.
+    // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+    public void ConfigureServices(IServiceCollection services)
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+        services.AddMvc()
+.AddJsonOptions
+        (
+        options =>
         {
+            options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+            options.SerializerSettings.NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore;
+        })
+.SetCompatibilityVersion(
+        Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_2_1);
+
+        services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = "JwtBearer";
+            options.DefaultChallengeScheme = "JwtBearer";
+        }).AddJwtBearer("JwtBearer", options =>
+        {
+            options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+            {
+                ValidateIssuer = true,
+
+                ValidateAudience = true,
+
+                ValidateLifetime = true,
+
+                IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("autopecas-chave-autenticacao")),
+
+                ClockSkew = TimeSpan.FromMinutes(30),
+
+                ValidIssuer = "AutoPecas.WebApi",
+
+                ValidAudience = "AutoPecas.WebApi"
+            };
+        });
+
+        services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new Swashbuckle.AspNetCore.Swagger.Info
+            {
+                Title = "M_AutoPecas", Version = "v1" });
+            });
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader().AllowCredentials());
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,10 +73,18 @@ namespace Senai.AutoPecas.WebApi
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
+            app.UseCors("CorsPolicy");
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "M_AutoPecas API V1");
             });
+
+            app.UseAuthentication();
+
+            app.UseMvc();
         }
     }
 }
